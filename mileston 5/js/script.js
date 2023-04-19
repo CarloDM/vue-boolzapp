9c8970f4d8928b92fb28ed19e41553f3d9bd169a
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-19 18:02:35 +0200

    chats chronological order

--- mileston 5/js/script.js
+++ mileston 5/js/script.js
@@ -11,18 +11,18 @@ createApp({
 
       messages: [
       {
-      date: '10/01/2020 15:30:55',
+      date: '10/1/2020 15:30:55',
       message: 'Hai portato a spasso il cane?',
       status: 'sent',
 
       },
       {
-      date: '10/01/2020 15:50:00',
+      date: '10/1/2020 15:50:00',
       message: 'Ricordati di stendere i panni',
       status: 'sent'
       },
       {
-      date: '10/01/2020 16:15:22',
+      date: '10/1/2020 16:15:22',
       message: 'Tutto fatto!',
       status: 'received'
       }
@@ -34,17 +34,17 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '20/03/2020 16:30:00',
+      date: '20/3/2020 16:30:00',
       message: 'Ciao come stai?',
       status: 'sent'
       },
       {
-      date: '20/03/2020 16:30:55',
+      date: '20/3/2020 16:30:55',
       message: 'Bene grazie! Stasera ci vediamo?',
       status: 'received'
       },
       {
-      date: '20/03/2020 16:35:00',
+      date: '20/3/2020 16:35:00',
       message: 'Mi piacerebbe ma devo andare a fare la spesa.',
       status: 'sent'
       }
@@ -56,17 +56,17 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '28/03/2020 10:10:40',
+      date: '28/3/2020 10:10:40',
       message: 'La Marianna va in campagna',
       status: 'received'
       },
       {
-      date: '28/03/2020 10:20:10',
+      date: '28/3/2020 10:20:10',
       message: 'Sicuro di non aver sbagliato chat?',
       status: 'sent'
       },
       {
-      date: '28/03/2020 16:15:22',
+      date: '28/3/2020 16:15:22',
       message: 'Ah scusa!',
       status: 'received'
       }
@@ -78,12 +78,12 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '10/01/2020 15:30:55',
+      date: '10/1/2020 15:30:55',
       message: 'Lo sai che ha aperto una nuova pizzeria?',
       status: 'sent'
       },
       {
-      date: '10/01/2020 15:50:00',
+      date: '10/1/2020 15:50:00',
       message: 'Si, ma preferirei andare al cinema',
       status: 'received'
       }
@@ -95,12 +95,12 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '10/01/2020 15:30:55',
+      date: '10/1/2020 15:30:55',
       message: 'Ricordati di chiamare la nonna',
       status: 'sent'
       },
       {
-      date: '10/01/2020 15:50:00',
+      date: '10/1/2020 15:50:00',
       message: 'Va bene, stasera la sento',
       status: 'received'
       }
@@ -112,17 +112,17 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '10/01/2020 15:30:55',
+      date: '10/1/2020 15:30:55',
       message: 'Ciao Claudia, hai novità?',
       status: 'sent'
       },
       {
-      date: '10/01/2020 15:50:00',
+      date: '10/1/2020 15:50:00',
       message: 'Non ancora',
       status: 'received'
       },
       {
-      date: '10/01/2020 15:51:00',
+      date: '10/1/2020 15:51:00',
       message: 'Nessuna nuova, buona nuova',
       status: 'sent'
       }
@@ -134,12 +134,12 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '10/01/2020 15:30:55',
+      date: '10/1/2020 15:30:55',
       message: 'Fai gli auguri a Martina che è il suo compleanno!',
       status: 'sent'
       },
       {
-      date: '10/01/2020 15:50:00',
+      date: '10/1/2020 15:50:00',
       message: 'Grazie per avermelo ricordato, le scrivo subito!',
       status: 'received'
       }
@@ -151,17 +151,17 @@ createApp({
       visible: true,
       messages: [
       {
-      date: '10/01/2020 15:30:55',
+      date: '10/1/2020 15:30:55',
       message: 'Ciao, andiamo a mangiare la pizza stasera?',
       status: 'received'
       },
       {
-      date: '10/01/2020 15:50:00',
+      date: '10/1/2020 15:50:00',
       message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
       status: 'sent'
       },
       {
-      date: '10/01/2020 15:51:00',
+      date: '10/1/2020 15:51:00',
       message: 'OK!!',
       status: 'received'
       }
@@ -214,6 +214,9 @@ createApp({
       this.contacts[this.chatIndex].messages.push(outMessage);
       this.stampDateTimeCompareNumb(this.chatIndex, this.contacts[this.chatIndex].messages.length - 1);
       console.log('key',this.contacts[this.chatIndex].messages);
+      this.stampCompare()
+      this.sortContacts();
+      this.activeChat(0);
     },
 
     searchingLog(){
@@ -248,18 +251,23 @@ createApp({
       hour = TimeForCompare[0]
       min = TimeForCompare[1]
       sec = TimeForCompare[2]
-      timeCompare = parseInt(hour + min + sec)
-      this.contacts[contactInd].messages[messageIndex].timeCompare = timeCompare
 
+      timeCompare = parseInt(hour + min + sec)
       date = dataTime[0]
       dateForCompare = date.split('/')
       day = dateForCompare[0]
-      mount = dateForCompare[1]
+
+      mount = '0'
+      if (parseInt(dateForCompare[1]) < 10 ){
+        mount = '0' + dateForCompare[1]
+      }else{mount = dateForCompare[1]};
+
       year = dateForCompare[2]
-      dateCompare = parseInt(year + mount + day)
-      this.contacts[contactInd].messages[messageIndex].dateCompare = dateCompare
-    
-      console.log(this.contacts[contactInd].messages[messageIndex],timeCompare , dateCompare )
+
+      compareInd = parseInt(year + mount + day + hour + min + sec)
+      this.contacts[contactInd].messages[messageIndex].compareInd = compareInd
+      
+      console.log(this.contacts[contactInd].messages[messageIndex],compareInd )
     },
 
     stampCompare(){
@@ -271,12 +279,23 @@ createApp({
           this.stampDateTimeCompareNumb(conta,mes)        
         }       
       }
-      // this.stampDateTimeCompareNumb(0,1)
+    },
+
+    sortContacts(){
+      console.log('sort')
+
+      this.contacts.sort((a,b) =>
+      (a.messages[a.messages.length - 1].compareInd  < 
+        b.messages[b.messages.length - 1].compareInd) ? 1 : 
+
+      (a.messages[a.messages.length - 1].compareInd> 
+        b.messages[b.messages.length - 1].compareInd ) ? -1 : 0);
     }
 
   },
   mounted(){
-    this.stampCompare()
+    this.stampCompare();
+    this.sortContacts();
 
   }
 }).mount('#app')
